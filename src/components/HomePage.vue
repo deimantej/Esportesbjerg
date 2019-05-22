47a07f294dfd0056de0e5a4ea44651a15d66de87
hzrbni <hzrbni@gmail.com>
2019-05-23 01:29:08 +0200

    - Info position is fixed - Carousel scroll and transition are fixed

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container fluid text-xs-center style="max-height: 100vh" id="header-carousel">
+  <v-container fluid text-xs-center id="header-carousel">
     <div class="images">
       <v-layout row wrap justify-center>
         <v-flex class="sponsor_item" xs4 d-flex v-for="(obj,key) in sponsors" :key="key">
@@ -7,7 +7,7 @@
         </v-flex>
       </v-layout>
     </div>
-    <v-carousel hide-delimiters hide-controls style="height: 100%">
+    <v-carousel hide-delimiters hide-controls>
       <v-carousel-item
         v-for="(item, i) in items"
         :src="item.img"
@@ -15,20 +15,22 @@
         reverse-transition="fade"
         transition="fade"
       >
-        <v-jumbotron dark  >
-          <v-container fill-height >
-            <v-layout row wrap align-center >
-              <v-flex xs12 d-block  >
-                <h3 class="display-1">{{ item.title }}</h3>
-                <v-btn round light class="blue--text" style="min-width:300px; border:2px solid #2196F3 ;" >Become a member</v-btn>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-jumbotron>
+        <v-container fill-height>
+          <v-layout row wrap align-center>
+            <v-flex xs12 d-block>
+              <h3 class="display-1 white--text">{{ item.title }}</h3>
+              <v-btn
+                round
+                light
+                class="blue--text"
+                style="min-width:300px; border:2px solid #2196F3 ;"
+              >Become a member</v-btn>
+            </v-flex>
+          </v-layout>
+        </v-container>
       </v-carousel-item>
     </v-carousel>
   </v-container>
-
 </template>
 
 
@@ -43,17 +45,17 @@ export default {
           text: " hello world"
         },
         {
-          img: require("@/assets/pictures/gamer.png"),
+          img: require("@/assets/pictures/gamer01.jpg"),
           title: "something",
           text: " hello world"
         },
         {
-          img: require("@/assets/pictures/gamer.png"),
+          img: require("@/assets/pictures/gamer02.jpg"),
           title: "something",
           text: " hello world"
         },
         {
-          img: require("@/assets/pictures/gamer.png"),
+          img: require("@/assets/pictures/gamer03.jpg"),
           title: "something",
           text: " hello world"
         }
@@ -94,5 +96,19 @@ export default {
   padding: 0;
 }
 
+#header-carousel .fade-enter-active,
+#header-carousel .fade-leave-active,
+#header-carousel .fade-leave-to {
+  transition: all 0.3s ease-out;
+  position: absolute;
+  top: 0;
+  width: 100%;
 
+  left: 0;
+}
+#header-carousel .fade-enter,
+#header-carousel .fade-leave,
+#header-carousel .fade-leave-to {
+  opacity: 0;
+}
 </style>
