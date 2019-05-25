6900b203e61edb7e5913461aab39199ec9dc9b9b
hzrbni <hzrbni@gmail.com>
2019-05-25 04:37:54 +0200

    Gallery styling + modal on click

--- src/components/AppGallery.vue
+++ src/components/AppGallery.vue
@@ -1,99 +1,68 @@
 <template>
-  <v-layout>
-    <v-flex xs12 sm6 offset-sm3>
+  <v-container grid-list-md>
+    <v-layout row wrap>
+      <v-flex v-for="(obj,key) in images" :key="key" xs6 sm4 md3>
+        <v-card flat>
+          <v-img :src="obj.img" @click="zoom(obj.img)" aspect-ratio="1"/>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <v-dialog v-model="dialog" max-width="90%">
       <v-card>
-        <v-container grid-list-sm fluid>
-          <v-layout row wrap>
-            <v-flex
-              v-for="(obj,key) in images" :key="key"
-              xs4
-              d-flex
-            >
-              <v-card flat tile class="d-flex">
-                <v-img
-                  v-bind:src="obj.img"
-                  contain
-                  aspect-ratio="1"
-                  class="transparent"
-                >
-                  <template v-slot:placeholder>
-                    <v-layout
-                      fill-height
-                      align-center
-                      justify-center
-                      ma-0
-                    >
-                      <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-                    </v-layout>
-                  </template>
-                </v-img>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-container>
+        <v-img :src="selectedImage" @click.stop="dialog = false" contain/>
       </v-card>
-    </v-flex>
-  </v-layout>
+    </v-dialog>
+  </v-container>
 </template>
 
 
 
 <script>
 export default {
-    data: () => ({
-      images: [
-            
-            {
-                img: require('@/assets/gallery/Træning ved Cosmos.jpg'),
-                
-            },
-            {
-                img: require('@/assets/gallery/Træning ved Cosmos_1.jpg'),
-                
-            },
-                       
-            {
-                img: require('@/assets/gallery/Træning ved Cosmos_2.jpg'),
-                
-            },
-            {
-                img: require('@/assets/gallery/Træning ved Cosmos_3.jpg'),
-                
-            },
-            {
-                img: require('@/assets/gallery/Træning ved Cosmos_4.jpg'),
-                
-            },
-            {
-                img: require('@/assets/gallery/Træning ved Cosmos_5.jpg'),
-                
-            },
-            {
-                img: require('@/assets/gallery/Træning ved Cosmos_6.jpg'),
-                
-            },
-            {
-                img: require('@/assets/gallery/Træning ved Cosmos_7.jpg'),
-                
-            },
-            {
-                img: require('@/assets/gallery/Træning ved Cosmos_8.jpg'),
-                
-            },
-            {
-                img: require('@/assets/gallery/Træning ved Cosmos_9.jpg'),
-                
-            },
-            {
-                img: require('@/assets/gallery/Træning ved Cosmos_2.jpg'),
-                
-            },
-
-
-        ],
-
-        
-        
-    })
+  data: () => ({
+    dialog: false,
+    selectedImage: "",
+    images: [
+      {
+        img: require("@/assets/gallery/Træning ved Cosmos.jpg")
+      },
+      {
+        img: require("@/assets/gallery/Træning ved Cosmos_1.jpg")
+      },
+      {
+        img: require("@/assets/gallery/Træning ved Cosmos_2.jpg")
+      },
+      {
+        img: require("@/assets/gallery/Træning ved Cosmos_3.jpg")
+      },
+      {
+        img: require("@/assets/gallery/Træning ved Cosmos_4.jpg")
+      },
+      {
+        img: require("@/assets/gallery/Træning ved Cosmos_5.jpg")
+      },
+      {
+        img: require("@/assets/gallery/Træning ved Cosmos_6.jpg")
+      },
+      {
+        img: require("@/assets/gallery/Træning ved Cosmos_7.jpg")
+      },
+      {
+        img: require("@/assets/gallery/Træning ved Cosmos_8.jpg")
+      },
+      {
+        img: require("@/assets/gallery/Træning ved Cosmos_9.jpg")
+      },
+      {
+        img: require("@/assets/gallery/Træning ved Cosmos_2.jpg")
+      }
+    ]
+  }),
+  methods: {
+    zoom(url) {
+      this.selectedImage = url;
+      this.dialog = true;
+    }
+  }
 };
 </script>
